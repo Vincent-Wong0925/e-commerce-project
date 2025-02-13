6f5a4c61a737bdbaad338da2ce44e166264c50a8
Vincent Wong <vincentwco2002@gmail.com>
2025-02-13 14:23:55 +0800

    Implement path to get a user by id

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const db = require('../db/index');
+const { validateId } = require('../utils');
 
 const usersRouter = express.Router();
+
+usersRouter.use('/:id', validateId);
 
 //Get all users
 usersRouter.get('/', async (req, res, next) => {
@@ -12,6 +15,25 @@ usersRouter.get('/', async (req, res, next) => {
         result = await db.query(queryString);
     } catch(err) {
         return res.status(400).send(err);
+    }
+    return res.send(result.rows);
+});
+
+//Get a user by id
+usersRouter.get('/:id', async (req, res, next) => {
+    const queryString = `
+    SELECT * FROM users
+    WHERE id = $1`;
+    let result;
+
+    try {
+        result = await db.query(queryString, [req.params.id]);
+    } catch(err) {
+        return res.status(400).send(err);
+    }
+    
+    if (result.rowCount == 0) {
+        return res.status(404).send('User not found');
     }
     return res.send(result.rows);
 });
