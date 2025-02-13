cf88c465edce32d1f56f476d1a4381e3cceafac5
Vincent Wong <vincentwco2002@gmail.com>
2025-02-13 13:59:38 +0800

    Implement path to post new user

--- routes/users.js
+++ routes/users.js
@@ -3,4 +3,23 @@ const db = require('../db/index');
 
 const usersRouter = express.Router();
 
+usersRouter.get('/', async (req, res, next) => {
+
+});
+
+//Add a new user to the database
+usersRouter.post('/', async (req, res, next) => {
+    const { username, password, email } = req.body;
+    const queryString = 
+    `INSERT INTO users (username, password, email)
+    VALUES ($1, $2, $3)`;
+    let result;
+    try {
+        result = await db.query(queryString, [username, password, email]);
+    } catch(err) {
+        return res.status(400).send(err);
+    }
+    return res.send('New user created');
+});
+
 module.exports = usersRouter;
