3d9fe9d035e8a91c10e2249b5aa3c9ab16c37439
Vincent Wong <vincentwco2002@gmail.com>
2025-02-13 13:46:04 +0800

    Create users router

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const db = require('./db/index');
 const express = require('express');
 const productsRouter = require('./routes/products');
+const usersRouter = require('./routes/users');
 
 
 const app = express();
@@ -9,6 +10,7 @@ const port = 3000;
 app.use(express.json());
 
 app.use('/products', productsRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
