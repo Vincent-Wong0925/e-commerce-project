66d762bd7be0c828e0d8bbeb873cf45fac782c06
Vincent Wong <vincentwco2002@gmail.com>
2025-02-17 10:51:57 +0800

    Implement get path of orders

--- routes/orders.js
+++ routes/orders.js
@@ -1,7 +1,49 @@
 const express = require('express');
 const db = require('../db/index');
+const { validateId } = require('../utils');
 
 const ordersRouter = express.Router();
+
+ordersRouter.use('/:id', validateId);
+
+//Get all orders or orders by user_id(optional)
+ordersRouter.get('/', async (req, res, next) => {
+    const { user_id } = req.query;
+    if(isNaN(Number(user_id))) { return res.status(400).send('Invalid user_id'); }
+
+    const queryString = `
+    SELECT * FROM orders
+    ${user_id !== undefined ? `WHERE user_id = $1` : ``}`;
+    let queryValue = [];
+    if (user_id !== undefined) { queryValue.push(user_id); }
+
+    let result;
+    try {
+        result = await db.query(queryString, queryValue);
+    } catch(err) {
+        return res.status(400).send(err);
+    }
+
+    if(result.rowCount == 0) { return res.status(404).send('Order not found'); }
+    return res.send(result.rows);
+});
+
+//Get an order by id
+ordersRouter.get('/:id', async (req, res, next) => {
+    const queryString = `
+    SELECT * FROM orders
+    WHERE id = $1`;
+
+    let result;
+    try {
+        result = await db.query(queryString, [req.params.id]);
+    } catch(err) {
+        return res.status(400).send(err);
+    }
+
+    if(result.rowCount == 0) { return res.status(404).send('Order not found'); }
+    return res.send(result.rows);
+});
 
 //Add a new order
 ordersRouter.post('/', async (req, res, next) => {
