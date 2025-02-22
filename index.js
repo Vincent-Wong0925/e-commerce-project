08c9ce36056339bb065d1dcea58c3da9d93f0ed3
Vincent Wong <vincentwco2002@gmail.com>
2025-02-22 10:55:42 +0800

    Initialize passport and LocalStrategy

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const usersRouter = require('./routes/users');
 const cartsRouter = require('./routes/carts');
 const ordersRouter = require('./routes/orders');
 const registrationRouter = require('./routes/registration');
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const db = require("./db/index");
 
 
 const app = express();
@@ -17,6 +20,28 @@ app.use('/users', usersRouter);
 app.use('/carts', cartsRouter);
 app.use('/orders', ordersRouter);
 app.use('/register', registrationRouter);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(
+  async function(email, password, done) {
+    let result;
+    try {
+      result = await db.query('SELECT * FROM users WHERE email = $1', [email]);
+      if (result.rowCount == 0) {
+        return done(null, false, { message: "Username or password incorrect" });
+      }
+      const user = result.rows[0];
+      if (user.password !== password) {
+        return done(null, false, { message: "Username or password incorrect" });
+      }
+      return done(null, user);
+    } catch(err) {
+      return done(err);
+    }
+  }
+));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
